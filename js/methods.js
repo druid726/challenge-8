2c95a5717e1410b8510bfed4e003e5074a820b47
druid726 <pdoneill26@gmail.com>
2014-03-24 21:22:00 -0400

    challenge-8 working on jquery

--- js/methods.js
+++ js/methods.js
@@ -10,30 +10,53 @@ $(function(){
 	});
 		
 	//show
-	
-			
+	$("#b2").click(function(){
+		$("#p1").show(1000);
+	});
+				
 	//toggle
 	
+	$("#b3").click(function(){
+		$("#p1").toggle(1000);
+	});
 		
 	//css
 	
-		
+	$("#b4").click(function(){
+		$("#p2").css("background-color", "#99CCCC");
+	});
+
 	//css (multiple property changes)
-	
-		
+
+	$("#b5").click(function(){
+		$("#p2").css( {"background-color": "#99CCCC", "font-weight": "bold", "color": "#fff", "padding": "5px" } );
+	});
+
 	//html
+	$("#b6").click(function(){
+		$("#p3").html("I don't get the purpose??");
+	});
 	
 		
 	//prepend
+	$("#b7").click(function(){
+		$("#p3").prepend("<strong> Wassup!</strong>  ");
+	});
 	
-		 
 	//append
-	
+	$("#b8").click(function(){
+		$("#p3").append("<strong> See ya!</strong>  ");
+	});
 		 
 	//before
-	
+	$("#b9").click(function(){
+		$("#p4").before("<h1> This is cool </h1>  ");
+	});
 		 
 	//after
+	$("#b10").click(function(){
+		$("#p4").after("<h1> Don't really understand why it's done this way </h1>  ");
+	});
 	
 		 
 	//wrap
